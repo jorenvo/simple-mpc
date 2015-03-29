55a51a6ca94accac8f02f48a5b38130d3e1901db
Joren Van Onder <joren.vanonder@gmail.com>
2015-03-29 13:09:46 +0200

    Add preamble and license info.

--- simple-mpc.el
+++ simple-mpc.el
@@ -1,3 +1,30 @@
+;;; simple-mpc.el -- part of simple-mpc, providing a simple interface to mpc
+;;
+;; Copyright (C) 2015
+
+;; Author: Joren Van Onder <joren.vanonder@gmail.com>
+;; Maintainer: Joren Van Onder <joren.vanonder@gmail.com>
+;; Keywords: multimedia, mpd, mpc
+;; Version: 1.0
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
 (require 'simple-mpc-current-playlist)
 (require 'simple-mpc-query)
 (require 'simple-mpc-vars)
