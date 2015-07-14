722aeefacc325b152582fab1ef31b36416d73ec6
Joren Van Onder <joren.vanonder@gmail.com>
2015-07-14 19:57:53 +0200

    Rename partial-mpc -> simple-mpc-partial-mpc

--- simple-mpc-utils.el
+++ simple-mpc-utils.el
@@ -28,15 +28,15 @@
 
 (require 'simple-mpc-vars)
 
-(defun partial-mpc (destination action)
+(defun simple-mpc-partial-mpc (destination action)
   (apply-partially 'call-process "mpc" nil destination nil action))
 
 (defun call-mpc (destination action &optional mpc-args)
   "Calls mpc with `call-process'. DESTINATION will be passed to
 `call-process' and MPC-ARGS will be applied to it."
   (if (listp mpc-args)
-      (apply (partial-mpc destination action) mpc-args)
-    (funcall (partial-mpc destination action) mpc-args)))
+      (apply (simple-mpc-partial-mpc destination action) mpc-args)
+    (funcall (simple-mpc-partial-mpc destination action) mpc-args)))
 
 (defun simple-mpc-get-current-playlist-position ()
   (with-temp-buffer
