jorenvo/simple-mpc

commit 89440b017d74fd675f22e0505ba8d86eabf0bc5a

Respect simple-mpc-playlist-format in simple-mpc-query
Before this there was no format given to mpc when searching. By default
mpc then uses the %file% metadata delimiter. This is fine if you use MPD
and you have organized, descriptive filenames. Mopidy however changes
the format of this delimiter to be:

urlencode(backend:type:identifier)

Because for e.g. Spotify results there is no real 'file'. Instead there
is a unique identifier. This unfortunately meant that although searching
worked with Mopidy as a server instead of MPD, you ended up with a query
buffer looking like:

...
spotify:artist:0fDF0jjmdouCIeWhNnblwV
spotify:artist:5EWpmduD1uxFH0D9uYVyJg
spotify:artist:6khZGp3Cq9nGdWnYUwJGxu
spotify:album:0LBQdWnuV0CAXyPIngb0UX
spotify:album:0TNvR6Co87E75jWohyn2jq
spotify:album:52SHWfP9ggb7cg1E0iBH1h
...

which isn't very useful.

To solve this problem we pass the user-defined
simple-mpc-playlist-format to 'mpc search'. The main issue with
this (and the main reason this was not done earlier) is that then you
cannot directly pass the selected lines in the query buffer to 'mpc
add'. For the add command to work correctly you have to pass the %file%
of the result.

To allow for this simple-mpc-query-current-result-alist was
introduced. It contains a (simple-mpc-playlist-format . %file%) pair for
every result in the query. This way we can look up %file% for whatever
the user selects.

To accomplish this in one 'mpc search' we prepend the %file% metadata
delimiter to simple-mpc-playlist-format, delimited by (simple-mpc). We
then extract out of the results the (simple-mpc-playlist-format
. %file%) pairs for simple-mpc-query-current-result-alist.

If you happen to have two results with the exact same name in your query
buffer this approach won't work, but hopefully that shouldn't happen.

simple-mpc-query.el
@@ -30,6 +30,10 @@
 (require 'simple-mpc-vars)
 (require 'simple-mpc-utils)
 
+(defvar simple-mpc-query-current-result-alist nil
+  "An association list containing a (`simple-mpc-playlist-format'
+. %file%) pair for every result in the latest query.")
+
 (define-minor-mode simple-mpc-query-mode
   "Minor mode for the simple-mpc-query screen.
 \\{simple-mpc-query-mode-map}."
@@ -48,6 +52,23 @@
   (kill-buffer simple-mpc-query-buffer-name)
   (simple-mpc-switch-to-main-buffer))
 
+(defun simple-mpc-query-get-%file%-for-result (result)
+  (cdr (assoc result simple-mpc-query-current-result-alist)))
+
+(defun simple-mpc-query-build-result-alist (search-type search-query)
+  "Builds `simple-mpc-query-current-result-alist' according to
+SEARCH-TYPE and SEARCH-QUERY."
+  (setq simple-mpc-query-current-result-alist
+        (let* ((file-metadata-delimiter "(simple-mpc)")
+               (query-format (concat file-metadata-delimiter "%file%" file-metadata-delimiter simple-mpc-playlist-format)))
+          (mapcar (lambda (mpc-result)
+                    (let* ((matches (s-match (format "^%s\\(.*\\)%s\\(.*\\)" file-metadata-delimiter file-metadata-delimiter) mpc-result))
+                           (full-match (nth 0 matches))
+                           (file (nth 1 matches))
+                           (user-specified-format (nth 2 matches)))
+                      (cons user-specified-format file)))
+                  (simple-mpc-call-mpc-strings (list "--format" query-format "search" search-type search-query))))))
+
 (defun simple-mpc-query (search-type search-query)
   "Perform an mpc search. SEARCH-TYPE is a tag type, SEARCH-QUERY
 is the actual query."
@@ -58,17 +79,13 @@ is the actual query."
 				       "performer" "comment" "disc" "filename"
 				       "any") nil t)
     (read-string "Query: ")))
+  (simple-mpc-query-build-result-alist search-type search-query)
   (let ((buf (get-buffer-create simple-mpc-query-buffer-name)))
     (with-current-buffer buf
       (read-only-mode -1)
       (erase-buffer)
-      (simple-mpc-call-mpc t (list "search" search-type search-query))
+      (insert (mapconcat (lambda (result) (car result)) simple-mpc-query-current-result-alist "\n"))
       (goto-char (point-max))
-
-      ;; remove trailing newline (from mpc)
-      ;; delete-char errors when it can't delete, ignore that
-      (ignore-errors (delete-char -1))
-
       (goto-char (point-min))
       (simple-mpc-mode)
       (simple-mpc-query-mode)
@@ -98,11 +115,14 @@ current playlist. When PLAY is non-nil, immediately play them."
 	    (setq beginning-first-line-region (line-beginning-position))
 	    (simple-mpc-goto-line last-line-region)
 	    (setq end-last-line-region (line-end-position)))
-	  (simple-mpc-call-mpc nil (cons "add" (split-string (buffer-substring beginning-first-line-region
-                                                                               end-last-line-region)
-                                                             "\n" t)))
-	  (deactivate-mark))
-      (simple-mpc-call-mpc nil (list "add" (buffer-substring-no-properties (line-beginning-position) (line-end-position))))
+	  (simple-mpc-call-mpc nil (cons "add" (mapcar (lambda (result)
+                                                         (simple-mpc-query-get-%file%-for-result result))
+                                                       (split-string (buffer-substring-no-properties beginning-first-line-region
+                                                                                                     end-last-line-region)
+                                                                     "\n" t))))
+          (deactivate-mark))
+      (simple-mpc-call-mpc nil (list "add" (simple-mpc-query-get-%file%-for-result
+                                            (buffer-substring-no-properties (line-beginning-position) (line-end-position)))))
       (forward-line))
     (if play
 	(simple-mpc-call-mpc nil (list "play" (number-to-string (1+ current-amount-in-playlist)))))))

simple-mpc.el
@@ -7,6 +7,7 @@
 ;; Maintainer: Joren Van Onder <joren.vanonder@gmail.com>
 ;; Keywords: multimedia, mpd, mpc
 ;; Version: 1.0
+;; Package-Requires: ((s "1.10.0"))
 
 ;; This file is not part of GNU Emacs.
 ;;
@@ -27,6 +28,8 @@
 ;; See README.org
 
 ;;; Code:
+
+(require 's)
 
 (require 'simple-mpc-mode)
 (require 'simple-mpc-current-playlist)

commit c920da63cc2c5cbc56fb8eb87f260dfffba163f9

Handle empty simple-mpc-playlist-format in simple-mpc-query
89440b017d74fd675f22e0505ba8d86eabf0bc5a introduced a bug which occurs
when you do not set simple-mpc-playlist-format. It will then have it's
default value of "".

In this case you will end up with the query view displaying all your
results as empty strings. Everything will still work, so you can still
add/remove files to the current playlist, but the entire buffer will
appear blank. This issue does not occur in simple-mpc-current-playlist
because there we only pass the format parameter if
simple-mpc-playlist-format is set. In simple-mpc-query however, we
always pass format because of what was done in
89440b017d74fd675f22e0505ba8d86eabf0bc5a.

We use this opportunity to make the behavior of passing the format
parameter more consistent between simple-mpc-query and
simple-mpc-current-playlist. Now we always pass the format parameter in
both modes, opting to pass "%file%" if simple-mpc-playlist-format is not
set.

Fixes #4

simple-mpc-current-playlist.el
@@ -51,11 +51,6 @@
   (kill-buffer simple-mpc-current-playlist-buffer-name)
   (simple-mpc-switch-to-main-buffer))
 
-(defun simple-mpc-get-playlist-format ()
-  (if (> (length simple-mpc-playlist-format) 0)
-      (list "--format" simple-mpc-playlist-format)
-    '()))
-
 (defun simple-mpc-view-current-playlist (&optional ignore-auto noconfirm keep-point)
   "Views the current playlist.
 
@@ -72,7 +67,7 @@ position. Otherwise, move it to the current track in the playlist."
         (erase-buffer)
         (insert
          (simple-mpc-format-as-table (simple-mpc-call-mpc-string
-                                      (append (simple-mpc-get-playlist-format) '("playlist")))))
+                                      (append (list "--format" (simple-mpc-get-playlist-format)) '("playlist")))))
         (if keep-point
             (progn
               (simple-mpc-goto-line original-line)

simple-mpc-query.el
@@ -60,7 +60,7 @@
 SEARCH-TYPE and SEARCH-QUERY."
   (setq simple-mpc-query-current-result-alist
         (let* ((file-metadata-delimiter "(simple-mpc)")
-               (query-format (concat simple-mpc-playlist-format file-metadata-delimiter "%file%" file-metadata-delimiter)))
+               (query-format (concat (simple-mpc-get-playlist-format) file-metadata-delimiter "%file%" file-metadata-delimiter)))
           (mapcar (lambda (mpc-result)
                     (let* ((matches (s-match (format "^\\(.*\\)%s\\(.*\\)%s" file-metadata-delimiter file-metadata-delimiter) mpc-result))
                            (full-match (nth 0 matches))

simple-mpc-utils.el
@@ -88,5 +88,10 @@ a Lisp program."
         (buffer-string))
     result))
 
+(defun simple-mpc-get-playlist-format ()
+  (if (string= simple-mpc-playlist-format "")
+      "%file%"
+    simple-mpc-playlist-format))
+
 (provide 'simple-mpc-utils)
 ;;; simple-mpc-utils.el ends here