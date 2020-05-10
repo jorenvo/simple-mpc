df131c700bfb2ed7bdce8a2c793436904cc61c66
Sean Farley <sean@farley.io>
2020-05-10 16:14:38 -0700

    simple-mpc-utils: lint the doc strings

--- simple-mpc-utils.el
+++ simple-mpc-utils.el
@@ -29,12 +29,16 @@
 (require 'simple-mpc-vars)
 
 (defvar simple-mpc-arguments ""
-  "Extra arguments that will be given to mpc. This can be used to
-eg. make mpc connect to a UNIX socket with --host.")
+  "Extra arguments that will be given to mpc.
+
+This can be used to eg. make mpc connect to a UNIX socket with
+--host.")
 
 (defun simple-mpc-call-mpc (destination mpc-args)
-  "Calls mpc with `call-process'. DESTINATION will be passed to
-`call-process' and MPC-ARGS will be passed to the mpc program."
+  "Call mpc with `call-process'.
+
+DESTINATION will be passed to `call-process' and MPC-ARGS will be
+passed to the mpc program."
   (if (not (listp mpc-args))
       (setq mpc-args (list mpc-args)))
   (if (> (length simple-mpc-arguments) 0)
@@ -42,29 +46,36 @@ eg. make mpc connect to a UNIX socket with --host.")
   (apply 'call-process "mpc" nil destination nil mpc-args))
 
 (defun simple-mpc-call-mpc-strings (mpc-args)
-  "Wrapper around `simple-mpc-call-mpc'. Returns the output from
-it as a list of strings created by splitting the output on
+  "Wrapper around `simple-mpc-call-mpc-string'.
+
+Passes MPC-ARGS to `simple-mpc-call-mpc-string' and returns the
+output as a list of strings created by splitting the output on
 newlines."
   (split-string (simple-mpc-call-mpc-string mpc-args) "\n" t))
 
 (defun simple-mpc-call-mpc-string (mpc-args)
-  "Wrapper around `simple-mpc-call-mpc'. Returns the output from
-it as a string."
+  "Wrapper around `simple-mpc-call-mpc'.
+
+Passes MPC-ARGS to `simple-mpc-call-mpc' and returns the
+output as a string."
   (with-temp-buffer
     (simple-mpc-call-mpc t mpc-args)
     (buffer-string)))
 
 (defun simple-mpc-get-current-playlist-position ()
+  "Return the position, as a number, of the current song."
   (with-temp-buffer
     (simple-mpc-call-mpc t '("current" "-f" "%position%"))
     (string-to-number (buffer-string))))
 
 (defun simple-mpc-get-amount-of-songs-in-playlist ()
+  "Return the number of songs in the current playlist."
   (with-temp-buffer
     (simple-mpc-call-mpc t "playlist")
     (count-lines (point-min) (point-max))))
 
 (defun simple-mpc-message-current-volume ()
+  "Return the current volume."
   ;; Use "%s" as format-string. Otherwise message will
   ;; interpret the percent symbol in mpc's output as an
   ;; incomplete format specifier.
@@ -75,17 +86,19 @@ it as a string."
      (buffer-string))))
 
 (defun simple-mpc-goto-line (line-number)
-  "Go to beginning of line LINE-NUMBER. Safe to be called from
-a Lisp program."
+  "Go to beginning of line LINE-NUMBER.
+
+Safe to be called from a Lisp program."
   (goto-char (point-min))
   (forward-line (1- line-number)))
 
 (defun simple-mpc-switch-to-main-buffer ()
-  "Switches to the main mpc buffer."
+  "Switch to the main mpc buffer."
   (if (get-buffer simple-mpc-main-buffer-name)
       (switch-to-buffer simple-mpc-main-buffer-name)))
 
 (defun simple-mpc-format-as-table (result)
+  "Format RESULT as a table."
   (if simple-mpc-table-separator
       (with-temp-buffer
         (insert
@@ -102,13 +115,16 @@ a Lisp program."
     result))
 
 (defun simple-mpc-get-playlist-format ()
+  "Return `simple-mpc-playlist-format' or %file%, if blank."
   (if (string= simple-mpc-playlist-format "")
       "%file%"
     simple-mpc-playlist-format))
 
 (defun simple-mpc-convert-number-to-relative-string (number)
-  "Converts an integer NUMBER to a string prefixed with either -
-or +. This is useful for mpc commands like volume and seek."
+  "Convert an integer NUMBER to a prefixed string.
+
+The prefix is either - or +. This is useful for mpc commands
+like volume and seek."
   (let ((number-string (number-to-string number)))
     (if (> number 0)
 	(concat "+" number-string)
